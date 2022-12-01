e91ab37379d36499506b67375dc61822fd9988a3
QA36 <QA36@DESKTOP-AESHFBQ>
2022-12-01 19:44:54 +0100

    AddNewProductTest

--- SwagProject/SwagProject/Tests/BuyProductTest.cs
+++ SwagProject/SwagProject/Tests/BuyProductTest.cs
@@ -1,3 +1,4 @@
+using OpenQA.Selenium;
 using SwagProject.Driver;
 using SwagProject.Page;
 
@@ -7,21 +8,26 @@ namespace SwagProject.Tests
     {
         LoginPage loginPage;
         ProductPage productPage;
+        CardPage cardPage;
+
 
         [SetUp]
         public void Setup()
         {
             WebDrivers.Initialize();
-            loginPage = new LoginPage(); 
+            loginPage = new LoginPage();
             productPage = new ProductPage();
+            cardPage = new CardPage();
             
+
+
         }
-       
+
         [TearDown]
-         public void ClosePage()
-         {
+        public void ClosePage()
+        {
             WebDrivers.CleanUp();
-         }
+        }
 
 
 
@@ -34,6 +40,45 @@ namespace SwagProject.Tests
             productPage.AddTShrit.Click();
 
             Assert.That("2", Is.EqualTo(productPage.Cart.Text));
+        }
+
+        [Test]
+
+        public void TC02_SortProductByPrice_ShouldSortByHighPrice()
+        {
+            loginPage.Login("standard_user", "secret_sauce");
+            productPage.SelectOption("Price (high to low)");
+
+
+            Assert.That(productPage.SortByPrice.Displayed);
+        }
+        [Test]
+
+        public void TC03_GoToAboutPage_ShouldRedactToNewPage()
+        {
+            loginPage.Login("standard_user", "secret_sauce");
+            productPage.MenuClick.Click();
+            productPage.AboutClick.Click();
+
+            Assert.That("https://saucelabs.com/", Is.EqualTo(WebDrivers.Instance.Url));
+        }
+        [Test]
+
+        public void TC04_BuyProduct_ShouldBeFinishedShopping()
+        {
+            loginPage.Login("standard_user", "secret_sauce");
+            productPage.AddBackPac.Click();
+            productPage.AddTShrit.Click();
+            productPage.ShoppingCardClick.Click();
+            cardPage.Checkout.Click();
+            cardPage.FirstName.SendKeys("Milos");
+            cardPage.LastName.SendKeys("Stankovic");
+            cardPage.ZipCode.SendKeys("11000");
+            cardPage.ButtonContinue.Submit();
+            cardPage.ButtonFinish.Click();
+
+            Assert.That("THANK YOU FOR YOUR ORDER", Is.EqualTo(cardPage.OrderFinished.Text));
+            
         }
     }
 }
